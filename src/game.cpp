3ddb11104a5f35995bb9c393d615506cb1343b6d
Seth Kasmann <sakasmann1@cougars.ccis.edu>
2018-01-29 10:27:20 -0600

    Added an initial texture with test code, just to try and get a level map working.

--- src/game.cpp
+++ src/game.cpp
@@ -12,8 +12,10 @@ Game::Game()
 
 Game::~Game()
 {
+    SDL_DestroyTexture(texture_);
     SDL_DestroyRenderer(renderer_);
     SDL_DestroyWindow(window_);
+    IMG_Quit();
     SDL_Quit();
 }
 
@@ -23,6 +25,7 @@ bool Game::init()
     // Initialize members.
     window_ = nullptr;
     renderer_ = nullptr;
+    texture_ = nullptr;
 
     // Initialize SDL2.
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
@@ -57,28 +60,84 @@ bool Game::init()
         return false;
     }
 
+    texture_ = IMG_LoadTexture(renderer_, "assets/SpriteSheet.png");
+
+    if (texture_ == nullptr)
+    {
+        std::cout << "Could not create texture:" << IMG_GetError() << '\n';
+        success = false;
+    }
+
     return true;
 }
 
 
 /*
-The main game loop function.
+    The main game loop function.
 */
 void Game::mainLoop()
 {
+    // Beginning of test code.
+    int x = 0;
+    int y = 0;
+    std::vector<int> LevelOne = 
+    {
+        54, 54, 54, 45, 28, 29, 18, 19, 19, 19, 19, 19, 75, 20, 29, 30, 48, 54, 54, 54,
+        54, 54, 54, 47, 31, 32, 22, 22, 22, 22, 22, 22, 78, 22, 32, 33, 46, 54, 54, 54,
+        54, 54, 54, 47, 31, 32, 24, 25, 24, 24, 25, 26, 77, 25, 32, 33, 48, 54, 54, 54,
+        54, 54, 54, 47, 31, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 33, 48, 54, 54, 54,
+        54, 54, 54, 45, 31, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 33, 48, 54, 54, 54,
+        54, 54, 54, 47, 31, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 33, 46, 54, 54, 54,
+        54, 54, 54, 45, 31, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 33, 48, 54, 54, 54,
+        54, 54, 54, 47, 31, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 33, 46, 54, 54, 54,
+        54, 54, 54, 47, 31, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 33, 48, 54, 54, 54,
+        54, 54, 54, 47, 31, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 33, 48, 54, 54, 54,
+        54, 54, 80, 95, 31, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 33, 48, 54, 54, 54,
+        54, 82, 83, 83, 31, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 33, 48, 54, 54, 54,
+        54, 54, 97, 83, 31, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 33, 48, 54, 54, 54,
+        54, 54, 54, 97, 34, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 36, 46, 54, 54, 54,
+        54, 54, 54, 54, 40, 41, 42, 41, 40, 40, 40, 40, 40, 41, 40, 40, 54, 54, 54, 54
+    };
+
+    for (int i = 0; i < 110; ++i)
+    {
+        srcRects_.push_back({x, y, 256, 256});
+        x = (x + 256) % 2048;
+        if (x == 0)
+            y += 256;
+    }
+    x = 0;
+    y = 0;
+    for (int i = 0; i < LevelOne.size(); ++i)
+    {
+        tiles_.push_back(Entity({x, y, 32, 32}, srcRects_[LevelOne[i]]));
+        x = (x + 32) % W;
+        if (x == 0)
+            y += 32;
+    }
+    // End of test code.
+
     while (eventHandler())
     {
-
+        SDL_RenderClear(renderer_);
+        for (int i = 0; i < tiles_.size(); ++i)
+        {
+            SDL_RenderCopy(renderer_,
+                           texture_,
+                           &tiles_[i].getSrcRect(),
+                           &tiles_[i].getDstRect());
+        }
+        SDL_RenderPresent(renderer_);
     }
 }
 
 
 /*
-The main event loop function. Call the appropriate function
-with the necessary data for each event type.
+    The main event loop function. Call the appropriate function
+    with the necessary data for each event type.
 
-The return is only false when an event is found that closes
-or quits the program.
+    The return is only false when an event is found that closes
+    or quits the program.
 */
 bool Game::eventHandler()
 {
@@ -111,10 +170,10 @@ bool Game::eventHandler()
 
 
 /*
-Function to handle keyboard input.
-key: the SDL_Scancode: https://wiki.libsdl.org/SDL_Scancode
-state: true when pressed, false when released.
-repeat: true if it is a repeated press.
+    Function to handle keyboard input.
+       key: the SDL_Scancode: https://wiki.libsdl.org/SDL_Scancode
+     state: true when pressed, false when released.
+    repeat: true if it is a repeated press.
 */
 void Game::keyboardEvent(int key, bool state, bool repeat)
 {
@@ -123,16 +182,16 @@ void Game::keyboardEvent(int key, bool state, bool repeat)
 
 
 /*
-Function to handle mouse button events.
-x: the x location of the button press.
-y: the y location of the button press.
-button: the button  that was pressed.
-SDL_BUTTON_LEFT
-SDL_BUTTON_MIDDLE
-SDL_BUTTON_RIGHT
-SDL_BUTTON_X1
-SDL_BUTTON_X2
-state: true when pressed, false when released.
+    Function to handle mouse button events.
+         x: the x location of the button press.
+         y: the y location of the button press.
+    button: the button  that was pressed.
+            SDL_BUTTON_LEFT
+            SDL_BUTTON_MIDDLE
+            SDL_BUTTON_RIGHT
+            SDL_BUTTON_X1
+            SDL_BUTTON_X2
+     state: true when pressed, false when released.
 */
 void Game::mouseButtonEvent(int x, int y, uint8_t button, bool state)
 {
--- src/game.h
+++ src/game.h
@@ -2,7 +2,11 @@
 #define GAME_H
 
 #include <iostream>
+#include <algorithm>
+#include <vector>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "entity.h"
 
 const int W = 640;
 const int H = 480;
@@ -18,8 +22,11 @@ public:
     void keyboardEvent(int key, bool state, bool repeat);
     void mouseButtonEvent(int x, int y, uint8_t button, bool state);
 private:
+    std::vector<Entity> tiles_;
+    std::vector<SDL_Rect> srcRects_;
     SDL_Window * window_;
     SDL_Renderer * renderer_;
+    SDL_Texture * texture_;
 };
 
 #endif

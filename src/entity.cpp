64bbf12c303309b7c6bfafa74e43c73b9a399960
Seth Kasmann <sakasmann1@cougars.ccis.edu>
2018-01-30 11:25:43 -0600

    Removed src rect from entity... since not all objects will have a src rect.

--- src/entity.cpp
+++ src/entity.cpp
@@ -1,36 +1,18 @@
-#include <SDL2/SDL.h>
 #include "entity.h"
 
 Entity::Entity()
-    : Entity({0, 0, 1, 1}, {0, 0, 1, 1})
+    : Entity({0, 0, 1, 1})
 {}
 
 
 Entity::Entity(const SDL_Rect& dst)
-    : Entity(dst, {0, 0, 1, 1})
+    : dst_(dst)
 {}
-
-
-Entity::Entity(const SDL_Rect& dst, const SDL_Rect& src)
-    : dst_(dst), src_(src)
-{}
-
-
-const SDL_Rect& Entity::getSrcRect() const
-{
-    return src_;
-}
 
 
 const SDL_Rect& Entity::getDstRect() const
 {
     return dst_;
-}
-
-
-void Entity::setSrcRect(const SDL_Rect& src)
-{
-    src_ = src;
 }
 
 
--- src/entity.h
+++ src/entity.h
@@ -1,6 +1,8 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
+#include <SDL2/SDL.h>
+
 /*
     Class to model a game entity. This keeps track of
     the source rect(the object's location on the texture),
@@ -8,20 +10,14 @@
     screen).
 */
 
-class SDL_Rect;
-
 class Entity
 {
 public:
     Entity();
     Entity(const SDL_Rect& dst);
-    Entity(const SDL_Rect& dst, const SDL_Rect& src);
-    const SDL_Rect& getSrcRect() const;
     const SDL_Rect& getDstRect() const;
-    void setSrcRect(const SDL_Rect& src);
     void setDstRect(const SDL_Rect& dst);
-private:
-    SDL_Rect src_;
+protected:
     SDL_Rect dst_;
 };
 

3bb8a1a5989d556cfaac3bd97b988db98d285411
Seth Kasmann <sakasmann1@cougars.ccis.edu>
2018-01-30 11:29:39 -0600

    Added a vector of Sprites to the level class. Perhaps we can implement a function to generate a level within this class. Commented code is just an example and will not compile.

--- src/level.cpp
+++ src/level.cpp
@@ -8,6 +8,41 @@ Level::Level(std::vector<int> data)
 {}
 
 
+void Level::generate(SDL_Texture* texture, 
+                     const std::vector<int>& data, 
+                     int clipWidth, 
+                     int clipHeight, 
+                     int tileWidth, 
+                     int tileHeight)
+{
+    /*
+    // Check the texture is not null.
+    if (texture == nullptr)
+        return;
+
+    tiles_.clear();
+
+    int texW, texH = 0;
+    SDL_QueryTexture(texture_, nullptr, nullptr, &texW, &texH);
+
+    SDL_Rect src = {0, 0, clipWidth, clipHeight};
+    SDL_Rect dst = {0, 0, tileWidth, tileHeight};
+
+    std::for_each(data.begin(), data.end(),
+        [&, src, dst](int i) : mutable
+        {
+            src.x = clipWidth * i % texW;
+            src.y = (clipWidth * i / texW) * clipHeight;
+            dst.x = (dst.x + tileWidth) % Width;
+            if (dst.x == 0)
+                dst.y += tileHeight;
+            tiles_.push_back(Sprite(dst, src, texture));
+        }
+    );
+    */
+}
+
+
 std::vector<int> Level::getData()
 {
     return data_;
--- src/level.h
+++ src/level.h
@@ -2,16 +2,19 @@
 #define LEVEL_H
 
 #include <vector>
+#include "sprite.h"
 
 class Level
 {
 public:
     Level();
     Level(std::vector<int>);
-
+    void generate(SDL_Texture* texture, const std::vector<int>& data, 
+        int clipWidth, int clipHeight, int tileWidth, int tileHeight);
     std::vector<int> getData();
     void setData(std::vector<int>);
 private:
+    std::vector<Sprite> tiles_;
     std::vector<int> data_;
 };
 
